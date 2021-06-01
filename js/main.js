6373bff097605c675c95e301bf6e3e4c0a10d62a
Douglas1800 <lukas.stock@hotmail.com>
2021-06-01 15:27:18 +0200

    partage + meta + json td

--- js/main.js
+++ js/main.js
@@ -8,3 +8,22 @@ document.onkeydown = function (e) {
 		document.getElementById("right").click();
 	}
 };
+
+function CopyToClipboard(node) {
+  var input = document.querySelector(node.getAttribute("data-target"))
+  node.addEventListener("click", this.clicked.bind(this))
+  this.input = document.createElement("input")
+  this.input.type = "text"
+  this.input.value = node.getAttribute("data-value")
+  this.input.setAttribute("readonly", true)
+  this.input.style.position = "absolute"
+  this.input.style.left = "-10000px"
+  node.appendChild(this.input)
+}
+
+CopyToClipboard.prototype.clicked = function() {
+  this.input.select()
+  document.execCommand("copy")
+
+  FlashMessages.notice("Copié !")
+}
